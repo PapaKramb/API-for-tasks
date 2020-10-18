0e0774b6d4983759918bb932f4e1c94fc74dc5df
PapaKramb <guccigang5554@gmail.com>
2020-10-19 01:09:40 +0900

    Update controllers and views

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,26 @@
 class TodosController < ApplicationController
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @project = Project.find(params[:project_id])
+    @todo = @project.todos.new(todo_params)
+    if @todo.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @todo = Todo.find(params[:todo_id])
+    @todo.update(todo_params)
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:text, :project_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'todos/new', to: 'todos#new', as: 'todos'
+
+  resources :projects do
+    resource :todos
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
